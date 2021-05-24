a208e569f5746984e48eb4ae574ce3ded088b6f2
Cass Agius <cassanda.agius@gmail.com>
2021-05-24 17:54:23 +1000

    Add project files.

---
+++ src/Patches.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+
+namespace WolvesComeOutAtNight
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(SpawnRegion), "Start")]
+        internal class ChangeWolfSpawns
+        {
+            private static void Postfix(SpawnRegion __instance)
+            {
+                if (__instance.m_AiSubTypeSpawned == AiSubType.Wolf)
+                { 
+                    GetSettings(__instance);
+                }
+            }
+        }
+
+        private static void GetSettings(SpawnRegion spawnRegion)
+        {
+            string activeScene = GameManager.m_ActiveScene;
+            if (activeScene is null) return;
+
+            if (activeScene == "AshCanyonRegion" && (Settings.settings.everywhere || Settings.settings.ashCanyon)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "CanneryRegion" && (Settings.settings.everywhere || Settings.settings.bleakInlet)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "CoastalRegion" && (Settings.settings.everywhere || Settings.settings.coastalHighway)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "CrashMountainRegion" && (Settings.settings.everywhere || Settings.settings.timberwolfMountain)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "DamRiverTransitionZoneB" && (Settings.settings.everywhere || Settings.settings.windingRiver)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "HighwayTransitionZone" && (Settings.settings.everywhere || Settings.settings.crumblingHighway)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "LakeRegion" && (Settings.settings.everywhere || Settings.settings.mysteryLake)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "MarshRegion" && (Settings.settings.everywhere || Settings.settings.forlornMuskeg)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "MountainTownRegion" && (Settings.settings.everywhere || Settings.settings.mountainTown)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "RiverValleyRegion" && (Settings.settings.everywhere || Settings.settings.hushedRiver)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "RuralRegion" && (Settings.settings.everywhere || Settings.settings.pleasantValley)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "TracksRegion" && (Settings.settings.everywhere || Settings.settings.brokenRailroad)) NoneDuringDay(spawnRegion);
+            else if (activeScene == "WhalingStationRegion" && (Settings.settings.everywhere || Settings.settings.desolationPoint)) NoneDuringDay(spawnRegion);
+        }
+
+        private static void NoneDuringDay(SpawnRegion spawnRegion)
+        {
+            spawnRegion.m_MaxSimultaneousSpawnsDayInterloper = 0;
+            spawnRegion.m_MaxSimultaneousSpawnsDayPilgrim = 0;
+            spawnRegion.m_MaxSimultaneousSpawnsDayStalker = 0;
+            spawnRegion.m_MaxSimultaneousSpawnsDayVoyageur = 0;
+        }
+    }
+}
---
+++ src/Settings.cs
@@ -0,0 +1,99 @@
+﻿using System.IO;
+using System.Reflection;
+using ModSettings;
+
+namespace WolvesComeOutAtNight
+{
+    class WolvesComeOutAtNightSettings : JsonModSettings
+    {
+        [Section("Wolves Only Come Out At Night")]
+        [Name("Everywhere")]
+        [Description("If you select this option, wolves will only come out at night in all regions")]
+        public bool everywhere = false;
+
+        [Name("Ash Canyon")]
+        [Description("Wolves only come out at night")]
+        public bool ashCanyon = false;
+
+        [Name("Bleak Inlet")]
+        [Description("Wolves only come out at night")]
+        public bool bleakInlet = false;
+
+        [Name("Broken Railroad")]
+        [Description("Wolves only come out at night")]
+        public bool brokenRailroad = false;
+
+        [Name("Coastal Highway")]
+        [Description("Wolves only come out at night")]
+        public bool coastalHighway = false;
+
+        [Name("Crumbling Highway")]
+        [Description("Wolves only come out at night")]
+        public bool crumblingHighway = false;
+
+        [Name("Desolation Point")]
+        [Description("Wolves only come out at night")]
+        public bool desolationPoint = false;
+
+        [Name("Forlorn Muskeg")]
+        [Description("Wolves only come out at night")]
+        public bool forlornMuskeg = false;
+
+        [Name("Hushed River Valley")]
+        [Description("Wolves only come out at night")]
+        public bool hushedRiver = false;
+
+        [Name("Mountain Town")]
+        [Description("Wolves only come out at night")]
+        public bool mountainTown = false;
+
+        [Name("Mystery Lake")]
+        [Description("Wolves only come out at night")]
+        public bool mysteryLake = false;
+
+        [Name("Pleasant Valley")]
+        [Description("Wolves only come out at night")]
+        public bool pleasantValley = false;
+
+        [Name("Timberwolf Mountain")]
+        [Description("Wolves only come out at night")]
+        public bool timberwolfMountain = false;
+
+        [Name("Winding River")]
+        [Description("Wolves only come out at night")]
+        public bool windingRiver = false;
+
+        protected override void OnChange(FieldInfo field, object oldValue, object newValue)
+        {
+            if (field.Name == nameof(everywhere)) RefreshFields();
+        }
+
+        internal void RefreshFields()
+        {
+            SetFieldVisible(nameof(ashCanyon), !everywhere);
+            SetFieldVisible(nameof(bleakInlet), !everywhere);
+            SetFieldVisible(nameof(brokenRailroad), !everywhere);
+            SetFieldVisible(nameof(coastalHighway), !everywhere);
+            SetFieldVisible(nameof(crumblingHighway), !everywhere);
+            SetFieldVisible(nameof(desolationPoint), !everywhere);
+            SetFieldVisible(nameof(forlornMuskeg), !everywhere);
+            SetFieldVisible(nameof(hushedRiver), !everywhere);
+            SetFieldVisible(nameof(mountainTown), !everywhere);
+            SetFieldVisible(nameof(mysteryLake), !everywhere);
+            SetFieldVisible(nameof(pleasantValley), !everywhere);
+            SetFieldVisible(nameof(timberwolfMountain), !everywhere);
+            SetFieldVisible(nameof(windingRiver), !everywhere);
+        }
+    }
+
+internal static class Settings
+    {
+        internal static WolvesComeOutAtNightSettings settings;
+        internal static void OnLoad()
+        {
+            settings = new WolvesComeOutAtNightSettings();
+            settings.RefreshFields();
+            settings.AddToModSettings("Wolves Come Out At Night");
+        }
+    }
+}
