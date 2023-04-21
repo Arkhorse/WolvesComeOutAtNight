ed2af9591f3cc555f9e53c6f0837e77af218676b
Arkhorse <theillusivemman@gmail.com>
2023-04-21 16:46:54 -0300

    add overall mod toggle, commented out the desc and added section headers

--- src/Settings.cs
+++ src/Settings.cs
@@ -6,83 +6,91 @@ namespace WolvesComeOutAtNight
 {
     class WolvesComeOutAtNightSettings : JsonModSettings
     {
-        [Section("Wolves Only Come Out At Night")]
+        [Section("Quick Settings")]
+
+        [Name("Mod Toggle")]
+        [Description("If you happen to need to disable the mod entirely, set this to false")]
+        public bool modToggle = false;
+
         [Name("Everywhere")]
         [Description("If you select this option, wolves will only come out at night in all regions")]
         public bool everywhere = false;
 
+        [Section("Regions")]
+
         [Name("Ash Canyon")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool ashCanyon = false;
 
         [Name("Blackrock")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool blackrock = false;
 
         [Name("Blackrock Prison")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool blackrockPrison = false;
 
         [Name("Bleak Inlet")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool bleakInlet = false;
 
         [Name("Broken Railroad")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool brokenRailroad = false;
 
         [Name("Coastal Highway")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool coastalHighway = false;
 
         [Name("Crumbling Highway")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool crumblingHighway = false;
 
         [Name("Desolation Point")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool desolationPoint = false;
 
         [Name("Forlorn Muskeg")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool forlornMuskeg = false;
 
         [Name("Hushed River Valley")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool hushedRiver = false;
 
         [Name("Keeper's Pass North")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool keepersPassNorth = false;
 
         [Name("Keeper's Pass South")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool keepersPassSouth = false;
 
         [Name("Mountain Town")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool mountainTown = false;
 
         [Name("Mystery Lake")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool mysteryLake = false;
 
         [Name("Pleasant Valley")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool pleasantValley = false;
 
         [Name("Timberwolf Mountain")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool timberwolfMountain = false;
 
         [Name("Winding River")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool windingRiver = false;
 
         [Section("DLC")]
+        [Description("Regions added by DLC")]
 
         [Name("Forsaken Airfields")]
-        [Description("Wolves only come out at night")]
+        //[Description("Wolves only come out at night")]
         public bool AirfieldRegion = false;
 
 #pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
