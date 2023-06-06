49b1849756c8ba87b6a0b31c18948e8d7cdffe6e
Arkhorse <theillusivemman@gmail.com>
2023-06-06 20:51:02 -0300

    Create Patches.cs
    add bears

---
+++ VisualStudio/Patches/Patches.cs
@@ -0,0 +1,21 @@
+﻿namespace WolvesComeOutAtNight
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(SpawnRegion), "Start")]
+        internal class ChangeWolfSpawns
+        {
+            private static void Postfix(SpawnRegion __instance)
+            {
+                if (__instance.m_AiSubTypeSpawned == AiSubType.Wolf && Settings.Instance.HandleWolves)
+                {
+                    WolvesComeOutAtNight.GetSettings(__instance);
+                }
+                if (__instance.m_AiSubTypeSpawned == AiSubType.Bear && Settings.Instance.HandleBears)
+                {
+                    WolvesComeOutAtNight.GetSettings(__instance);
+                }
+            }
+        }
+    }
+}
