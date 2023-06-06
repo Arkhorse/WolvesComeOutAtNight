7121a7a57842bcbed8c5bddc8737400c9b35a23b
Arkhorse <theillusivemman@gmail.com>
2023-06-06 20:50:05 -0300

    Create BuildInfo.cs

---
+++ VisualStudio/BuildInfo.cs
@@ -0,0 +1,31 @@
+﻿namespace WolvesComeOutAtNight
+{
+    public static class BuildInfo
+    {
+        #region Mandatory
+        /// <summary>The machine readable name of the mod (no special characters or spaces)</summary>
+        public const string Name            = "WolvesComeOutAtNight";
+        /// <summary>Who made the mod</summary>
+        public const string Author          = "GruffCassquatch, The Illusion";
+        /// <summary>Current version (Using Major.Minor.Build) </summary>
+        public const string Version         = "1.2.4";
+        #endregion
+
+        #region Optional
+        /// <summary>What the mod does</summary>
+        public const string Description     = "Makes wolves only come out at night";
+        /// <summary>Company that made it</summary>
+        public const string Company         = null;
+        /// <summary>A valid download link</summary>
+        public const string DownloadLink    = null;
+        /// <summary>Copyright info</summary>
+        public const string Copyright       = "Copyright ©  2023";
+        /// <summary>Trademark info</summary>
+        public const string Trademark       = null;
+        /// <summary>Product Name (Generally use the Name)</summary>
+        public const string Product         = "WolvesComeOutAtNight";
+        /// <summary>Culture info</summary>
+        public const string Culture         = null;
+        #endregion
+    }
+}
