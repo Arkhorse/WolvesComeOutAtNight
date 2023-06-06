d8e7f0aa4315373cd5b6e39ed0b3ae1549f34e7e
Arkhorse <theillusivemman@gmail.com>
2023-06-06 20:50:20 -0300

    Create PredatorRegions.cs
    This is for a future update

---
+++ VisualStudio/PredatorRegions.cs
@@ -0,0 +1,26 @@
+﻿namespace WolvesComeOutAtNight
+{
+    internal enum PredatorRegions
+    {
+        AshCanyonRegion,
+        BlackrockRegion,
+        BlackrockPrisonSurvivalZone,
+        CanneryRegion,
+        TracksRegion,
+        CoastalRegion,
+        HighwayTransitionZone,
+        WhalingStationRegion,
+        MarshRegion,
+        RiverValleyRegion,
+        BlackrockTransitionZone,
+        CanyonRoadTransitionZone,
+        MountainTownRegion,
+        LakeRegion,
+        RuralRegion,
+        CrashMountainRegion,
+        DamRiverTransitionZoneB,
+        AirfieldRegion,
+        LongRailTransitionZone,
+        HubRegion
+    }
+}
